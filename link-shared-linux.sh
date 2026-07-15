34381157fb6a77976dac62a324e5d01ef9cb405d
Joshua L <Langerz82@users.noreply.github.com>
2026-07-15 18:03:11 +1000

    shared data in it's own directory now.

---
+++ link-shared-linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s ./shared client/shared
+ln -s ./shared gameserver/shared
+ln -s ./shared userserver/shared
---
+++ link-shared-win.bat
@@ -0,0 +1,3 @@
+mklink /J client\shared shared
+mklink /J gameserver\shared shared
+mklink /J userserver\shared shared
